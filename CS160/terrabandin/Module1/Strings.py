97e43983c6b4209ece5ed9b4aa9167a1b53afd0e
Terra Bandin <tkbandin@gmail.com>
2020-06-16 14:59:03 -0700

    Submission for Module 1

---
+++ CS160/terrabandin/Module1/Strings.py
@@ -0,0 +1,14 @@
+message = "this is a also a string"
+
+print("Title : " + message.title())
+
+print("Uppercase :" + message.upper())
+
+print("Lowercase :" + message.lower())
+
+# Concatenation
+
+first_message = "Hi !"
+second_message = "How are you ?"
+full_message = f"{first_message} {second_message}"
+print(full_message)
---
+++ CS160/terrabandin/Module1/variable.py
@@ -0,0 +1,10 @@
+print("\nMultiple Assignments\n")
+a, b, c = 5, 3.2, "Hello"
+print ("a = ", a)
+print ("b = ", b)
+print ("c = ", c)
+
+x = y = z = "Python"
+print ("x = " + x)
+print ("y = " + x)
+print ("z = " + x)
