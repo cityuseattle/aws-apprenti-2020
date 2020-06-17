0a35f38139a4a294c3e34212ac80edeb2466f2be
Wilson <lexon@3c22fb582218.ant.amazon.com>
2020-06-17 14:30:26 -0700

    Actual First Submission of HOP1

--- CS160/AlexWilson/Module01/HOP1_WilsonAlex.py
+++ CS160/AlexWilson/Module01/HOP1_WilsonAlex.py
@@ -1,1 +1,54 @@
-print("Hello World")
+# Prints hello world
+# print("Hello World")
+
+# variable exercise
+'''
+print("\nMultiple assignments\n")
+a, b, c, = 5, 3.2, "Hello"
+
+print ("a = ",a)
+print ("b = " ,b)
+print ("c = ",c)
+
+x = y = z = "Python"
+print ("x = "+x)
+print ("y = "+y)
+print ("z = "+z)
+'''
+
+# Data Types Exercise
+
+# There are three different types of data:
+
+# NUMERIC DATA
+## Integer, aka whole numbers eg. x = 5
+## Float, aka decimal numbers eg. y = 2.5
+## Complex Number, aka number with a 'real' and 'imaginary' componenet eg. z = 2 + 3i
+
+# BOOLEAN DATA
+## Boolean can be described as a piece of data with built
+## in values that are either TRUE or FALSE. For instance:
+'''
+print (10>9)
+'''
+# The output should read 'TRUE'.
+
+# SEQUENCE TYPE
+## There are two type of built in sequence data types:
+## String and List.
+# String types are denoted by ', ", '"
+'''
+message = "This is a string declaration"
+message = 'This is also a string declaration'
+'''
+# List types are denoted by [], with commas; a lists contents can be alter
+# that is to say it is 'mutable'
+'''
+list1 = ['physics', chemistry', 1997, 2000]
+'''
+
+# A Tuple is alot like a list, but is denoted by paranthasis
+# and it also immutable.
+'''
+tup1 = ('physics', 'chemistry', 1997, 2000)
+'''
