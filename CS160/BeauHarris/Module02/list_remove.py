99f7e3c6b325952107f97da5690270dba8141cad
Beau Harris <beau.harris@trojans.dsu.edu>
2020-06-17 15:36:56 -0600

    Work for Module 2

---
+++ CS160/BeauHarris/Module02/list_remove.py
@@ -0,0 +1,8 @@
+motorcycles = ['honda', 'yamaha', 'suzuki']
+popped_motorocycle = motorcycles.pop()
+
+print(motorcycles)
+print(popped_motorocycle)
+
+first_owned = motorcycles.pop(0)
+print("the first", first_owned)
