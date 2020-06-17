128444c31947c4a05c102d66cc4912792a729f37
DavidmWilner <david.m.willner@gmail.com>
2020-06-17 13:25:23 -0700

    submission for Module 2

---
+++ CS160/David-Willner/Module2/List_remove.py
@@ -0,0 +1,18 @@
+#del
+motorcycles = ['honda', 'yamaha', 'suzuki']
+del motorcycles[1]
+print(motorcycles)
+
+
+#pop
+motorcycles = ['honda', 'yamaha','suzuki']
+popped_motorcycles = motorcycles.pop()
+print(motorcycles)
+print(popped_motorcycles)
+first_owned = motorcycles.pop(0)
+print("The first motorcycle I owned was a", first_owned)
+
+#remove
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati' ]
+motorcycles.remove('ducati')
+print(motorcycles)
