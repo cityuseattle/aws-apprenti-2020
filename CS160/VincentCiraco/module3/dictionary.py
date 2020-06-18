32a2a9706e4d2a7e8826e1b718ab8c4402c4a21f
vincent <vinchenzociraco95@gmail.com>
2020-06-18 10:26:42 -0700

    Module 3 HOP

---
+++ CS160/VincentCiraco/module3/dictionary.py
@@ -0,0 +1,15 @@
+#accessing 
+dict = {'name': 'abc', 'Age': 7}
+print ("Name :", dict['Name'] , "\n" , "Age :", dict['Age'])
+
+#updating 
+dict['Age']=20
+print("Updating Age :", dict['Age'])
+
+#Adding
+dict['Phone_no']=123456789
+print("After adding the new pair :", dict)
+
+#Deleting
+del dict['Phone_no']
+print("After deleting phone_no :", dict)
