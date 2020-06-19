f570eaa56ea0673a60be1a9126cce2bf9826ec84
Terra Bandin <tkbandin@gmail.com>
2020-06-19 15:06:31 -0700

    Submission for Module 5

---
+++ CS160/terrabandin/Module5/File_mode.py
@@ -0,0 +1,11 @@
+file = open("hello.txt", "a")
+file.write("\nThis is new content I just appended")
+file.close()
+
+new_file = open("world.txt", "w")
+new_file.write("This is new file")
+new_file.close()
+
+file = open("hello.txt")
+content = file.read()
+print(content)
