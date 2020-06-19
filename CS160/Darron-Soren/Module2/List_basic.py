949fb5b42f4c747107b2f941d409ca80a8b2cba4
Darron Soren <cpttacsat@gmail.com>
2020-06-18 22:48:56 -0400

    Submission Module 2

---
+++ CS160/Darron-Soren/Module2/List_basic.py
@@ -0,0 +1,21 @@
+list1 = [ 'physics', 'chemistry', 1997, 2000]
+list2 = [1, 2, 3, 4, 5, 6, 7]
+
+#accessing
+print ( 'list1[0]: ', list1[0])
+print ( 'list2[1:5]: ', list2[1:5])
+
+#updating
+print ( 'Value available at index 2 : ')
+print ( list1[2] )
+list1[2] = 2001
+print ( 'New value avaialable at index 2 : ')
+print ( list1[2])
+
+#adding
+list1.append(2020)
+print ( 'NewList:', list1)
+
+#Insert
+list1.insert( 0, 'python')
+print ( 'After inserting: ', list1)
