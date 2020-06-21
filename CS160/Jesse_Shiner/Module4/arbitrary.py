1a7cca50b3fc3ef14764a7e043c98505c4644ae7
Shinerj <jshiner2018@gmail.com>
2020-06-20 20:12:38 -0700

    Module 4

---
+++ CS160/Jesse_Shiner/Module4/arbitrary.py
@@ -0,0 +1,7 @@
+# Arbitrary numbers
+
+def menu(*foods):
+    print(foods)
+
+menu("pizza")
+menu("soup", "fries", "pizza", "pasta")
---
+++ CS160/Jesse_Shiner/Module4/order_module.py
@@ -0,0 +1,7 @@
+#Order Module
+
+def order(number, *lists):
+    print(f"\nNumber of dishes: {number}, The dishes are:")
+    for list in lists:
+        print(f"-{list}")
+        
