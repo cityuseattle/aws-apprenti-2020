371a1adf71f5d72a5c6769a75ea0f5bbed95ab44
Jean-Jacques Philippe Guerrier <jean-jacquesphilippeguerrier@Jean-Jacquess-MacBook-Pro.local>
2020-06-22 23:21:03 -0400

    Submission for Module4

---
+++ CS160/GeraldGuerrier/Module4/keyword.py
@@ -0,0 +1,6 @@
+def describe_pet(animal_type , pet_name,color="brown"):
+    print(f"\nI have a {animal_type}.")
+    print(f"my {animal_type}'s name is {pet_name.title()}. It's color is {color}.")
+
+describe_pet(animal_type='hamster' , pet_name='harry')
+
---
+++ CS160/GeraldGuerrier/Module4/positional.py
@@ -0,0 +1,7 @@
+def describe_pet(animal_type, pet_name): 
+    print(f"\nI have a {animal_type}.")
+    print(f"my {animal_type}'s name is {pet_name.title()}'")
+
+describe_pet ('hammer' , 'harry')
+describe_pet ('dog' , 'wiilie')
+
