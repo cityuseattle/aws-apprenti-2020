b8e32d10bc827697c6cfb8cb6fa4ace8bbb06fbc
Jean-Jacques Philippe Guerrier <jean-jacquesphilippeguerrier@Jean-Jacquess-MacBook-Pro.local>
2020-06-23 01:56:39 -0400

    Submission for Module6

---
+++ CS160/GeraldGuerrier/Module6/init_oop.py
@@ -0,0 +1,15 @@
+# # name of the class normally use CapWords convention
+class Employee:
+
+    def __init__(self, first, last, pay):
+        # instance variable unique to each instance 
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + ' . ' + last + '@company.com'
+
+    def showinfo(self):
+        return '{} {}, {}'.format(self.first, self.last, self.email)
+        
+emp1 = Employee('Elliot' , 'Alderson' , 7000)
+emp2 = Employee('Jean' , 'Grey' , 8000)
---
+++ CS160/GeraldGuerrier/Module6/update_oop.py
@@ -0,0 +1,10 @@
+emp1.first = 'Elliot'
+emp1.last = 'Alderson'
+emp1.email = 'Elliot.Alderson@company.com'
+emp1.pay = 7000
+
+emp1.first = 'Jean'
+emp1.last = 'Grey'
+emp1.email = 'Jean.Grey@company.com'
+emp1.pay = 8000
+
