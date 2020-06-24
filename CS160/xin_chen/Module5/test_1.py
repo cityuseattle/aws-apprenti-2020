5cb815869466e42ddb0cc0ff2d518e5da4f9acca
Chen <xchen190325@gmail.com>
2020-06-23 19:30:51 -0700

    submission for Module5

--- CS160/xin_chen/Module5/test_1.py
+++ CS160/xin_chen/Module5/test_1.py
@@ -2,7 +2,7 @@ try:
     fh = open("testfile", "w")
     fh.write("This is my test file for exception handling!!")
 except IOError:
-    print("Error: can\'t find file or read data")
+    print("Error: can\'t  find file or read data")
 else:
     print("Written content in the file successfully")
     fh.close()
