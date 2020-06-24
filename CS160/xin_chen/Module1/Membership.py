0cda1c291b780671b13d15197ed33938130c966a
Chen <xchen190325@gmail.com>
2020-06-23 19:37:24 -0700

    submission for Module1

--- CS160/xin_chen/Module1/Membership.py
+++ CS160/xin_chen/Module1/Membership.py
@@ -3,7 +3,7 @@ b = 20
 list = [1, 2, 3, 4, 5]
 
 if a in list:
-    print("Line 1 - a is available in the given list")
+    print("Line 1 - a is available  in the given list")
 else:
     print("Line 1 - a is not available in the given list")
 
