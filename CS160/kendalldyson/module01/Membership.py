fbc2ef76018e5e572fbba6d81630de7e6674bd4c
Kendall Jay Dyson <kjdyson@Kendalls-MacBook-Air.local>
2020-06-25 12:15:28 -0700

    Submission for Module 1

---
+++ CS160/kendalldyson/module01/Membership.py
@@ -0,0 +1,20 @@
+a = 10
+b = 20
+list = [1, 2, 3, 4, 5 ]
+
+if ( a in list ):
+    print ("List 1-a is available in the given list ")
+else:
+     print ("Line 1-a is not avaibale in the given list") 
+  
+if ( b in list ):
+    print ("List 2-b is not available in the given list ")
+else:
+     print ("Line 2-b is  avaibale in the given list") 
+
+a =2 
+if ( a in list ):
+    print ("line 3 -a  is avaiable in the givne list ")
+else:
+    print ("Line 3-a is not available in the given list")
+  
