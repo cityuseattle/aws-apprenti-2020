a5073e1df4f7c8d3efc7d656a26852d76646af1b
Kendall Jay Dyson <kjdyson@Kendalls-MacBook-Air.local>
2020-06-26 07:25:07 -0700

    Submission for Module 4

---
+++ CS160/kendalldyson/module04/myfunc.py
@@ -0,0 +1,8 @@
+def printme ( str ):
+    print (str)
+    return
+
+#Now you can call printme function
+printme("First call to user defined function")
+printme("Second call to the same function")
+
