c189979e019de05d956f08d9cd667c62f691059d
Robert Kennedy <66082154+rjkennedy707@users.noreply.github.com>
2020-06-27 21:34:46 -0400

    Sbumission for Module 8

---
+++ CS160/Robert-Kennedy/Module8/write_excel.py
@@ -0,0 +1,18 @@
+import openpyxl
+
+wb = openpyxl.Workbook()
+sheet = wb.active
+sheet.title = "Cars"
+sheet['A1'] = "Models"
+sheet['B1'] = "Price"
+
+wb.save("car_data.xlsx")
+
+cars = [('BMW', 4000), ('Audi', 50000), ('Ford', 25000),
+        ('Mclaren', 1800000), ('Toyota', 30000)]
+
+for car in cars:
+    sheet.append(car)
+    #appends a group of values at the bottom of the current sheet
+    #in order from first column
+wb.save('car_data.xlsx')
