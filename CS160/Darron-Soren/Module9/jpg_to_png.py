840421d7378f3e400dff41d79d681a59f6b00683
Darron Soren <cpttacsat@gmail.com>
2020-06-28 14:56:36 -0400

    Module 9

---
+++ CS160/Darron-Soren/Module9/jpg_to_png.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+img = Image.open ( 'bulldog.jpg' )
+width, height = img.size
+print ( 'Width:', width, 'Height:', height )
+
+if img.format == 'JPEG':
+    img.save ( 'bulldog2.png')
+    
