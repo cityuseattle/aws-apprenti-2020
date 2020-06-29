215375472d82cdd34231d6c4ddd08c2c22904021
lanelane132 <djlane19@gmail.com>
2020-06-29 00:28:44 -0700

    LaneDerrell

--- CS160/README.md
+++ CS160/README.md
@@ -1,1 +1,2 @@
 # CS160
+def function1():
---
+++ CS160/userinput.py
@@ -0,0 +1,6 @@
+name = input ("Please enter your name: ")
+print ("\nWelcome to python, "+ name + "!")
+print("The type of the variable name is" ,type(name))
+age=input("\nEnter your age: ")
+print("\nYour age is "+age)
+print("the type of the variable age is " ,type(age))
