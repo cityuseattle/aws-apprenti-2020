5cb611d60589c92f1df8ea23617b15fc4d8c90d0
Robert Kennedy <66082154+rjkennedy707@users.noreply.github.com>
2020-06-28 21:33:49 -0400

    Submission for Module 9

---
+++ CS160/Robert-Kennedy/Module9/filter.py
@@ -0,0 +1,9 @@
+from PIL import Image, ImageFilter
+# blue image
+img = Image.open('bulldog2.png')
+im_blurred = img.filter(filter=ImageFilter.BLUR)
+im_blurred.save('blur.jpg')
+
+#grayscale
+img_gray = img.convert('L')
+img_gray.save('grayscale.png')
