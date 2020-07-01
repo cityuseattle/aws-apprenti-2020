5c7ceeff2fd81b16889f104ef8276bd74e71ea88
Wilson <lexon@3c22fb582218.ant.amazon.com>
2020-07-01 14:53:16 -0700

    adding in class scripts

---
+++ CS142/AlexWilson/In Class/9-16.js
@@ -0,0 +1,15 @@
+function calculateTax(amount) {
+    return amount * 1.2;
+}
+function writePrice(product, price) {
+    console.log('Price for ${product}: $${price.toFixed(2)');
+}
+var Product;
+(function (Product) {
+    Product[Product["Hat"] = 0] = "Hat";
+    Product[Product["Gloves"] = 20] = "Gloves";
+    Product[Product["Umbrella"] = 21] = "Umbrella";
+})(Product || (Product = {}));
+var productValue = 0;
+var productName = Product[productValue];
+console.log('Value: ${productalue}, Name: ${productName}');
