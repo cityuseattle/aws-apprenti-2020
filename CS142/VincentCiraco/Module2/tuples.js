e8cd9ec40a6d6a761af0daca95306d706a7c931f
vincent <vinchenzociraco95@gmail.com>
2020-07-01 15:58:52 -0700

    Commit Module2

---
+++ CS142/VincentCiraco/Module2/tuples.js
@@ -0,0 +1,14 @@
+var tupleA = ['Hello Tuple'];
+var tupleB = [135, 'rgb', true];
+var tupleC = [
+    [12, 34],
+    [56, 78, 25],
+    [90, 12]
+];
+var tupleD = [
+    [12, 34],
+    [56, 78, 25],
+    [90, 12]
+];
+var bools = [true, true, false, true, false];
+var info = ['Hello', 372, true, false, true, true];
---
+++ CS142/VincentCiraco/Module2/tuples.ts
@@ -0,0 +1,17 @@
+const tupleA: [string] = ['Hello Tuple'];
+const tupleB: [number, string, boolean] = [135, 'rgb', true];
+
+const tupleC: [number, number, number?][] = [
+    [12, 34],
+    [56,78,25],
+    [90,12]
+];
+const tupleD: ([number, number] | [number, number, number])[]= [
+    [12,34],
+    [56,78,25],
+    [90,12]
+];
+
+const bools: [boolean, ...boolean[]] = [true, true, false, true, false];
+
+const info: [string, number, ...boolean[]] = ['Hello', 372, true, false, true, true];
