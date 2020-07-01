034cd2c8f5bcd00eb768d375fe52f23e03d48797
Wilson <lexon@3c22fb582218.ant.amazon.com>
2020-07-01 13:29:00 -0700

    HOP1B Submission for Alex Wilson

---
+++ CS142/AlexWilson/In Class/1JULAM.js
@@ -0,0 +1,15 @@
+let myVar;
+console.log('${myVar} = ' + typeof myVar);
+myVar = 12;
+console.log('${myVar} = ' + typeof myVar);
+myVar = "Hello";
+console.log('${myVar} = ' + typeof myVar);
+myVar = true;
+console.log('${myVar} = ' + typeof myVar);
+
+console.log(1 + 1);
+console.log(1 + '2');
+let myVar;
+myVar2 = 1 + '2';
+console.log(myVar2);
+
