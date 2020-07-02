a56c7a74b72db11ad01fc4d350f85e2ae343e278
Hye Shin <hye.stanford2@gmail.com>
2020-07-02 11:48:22 -0700

    Submission for Module 3

---
+++ CS142/hyeshin/Module3/more_types.ts
@@ -0,0 +1,23 @@
+// Type Alias
+
+type Id = number;
+
+let employeeId: Id = 2413;
+let studentId: Id = 13722;
+
+console.log(typeof employeeId);
+console.log(typeof studentId);
+
+// Generic Type Parameter (Polymorphic Type Parameter)
+let arr = [
+    'E line',
+    14,
+    true
+];
+
+function filter<T>(input: T) {
+    console.log(typeof input);
+}
+
+arr.forEach(item => filter(item));
+
