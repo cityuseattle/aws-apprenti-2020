063477bd5d7af9082a8d50a281cb3c1768b949ca
zdvis <66082221+zdvis@users.noreply.github.com>
2020-07-02 15:22:43 -0700

    Submission for Module 3

---
+++ CS142/zachdavis/Module3/more_types.js
@@ -0,0 +1,23 @@
+var employeeId = 2413;
+var studentId = 13722;
+console.log(typeof employeeId);
+console.log(typeof studentId);
+
+let arr = [
+    'E line',
+    14,
+    true
+];
+
+function filter<T>(input: T) {
+    let var1: any;
+    let var2; T;
+    var1 = input;
+    var2 = input;
+    var1 = 'test';
+    var2 = 'test';
+    console.log(var1);
+    console.log(var2);
+}
+
+testType(a);
---
+++ CS142/zachdavis/Module3/more_types.ts
@@ -0,0 +1,7 @@
+type Id = number;
+
+let employeeId: Id = 2413;
+let studentId: Id = 13722;
+
+console.log(typeof employeeId);
+console.log(typeof studentId);
