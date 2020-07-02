f03a2d0a6713ca0a879d6a53b4e4efa7ef5b471a
Wilson <lexon@3c22fb582218.ant.amazon.com>
2020-07-02 12:31:37 -0700

    Submission HOP3 for Alex Wilson

---
+++ CS142/AlexWilson/In Class/CT3.js
@@ -0,0 +1,8 @@
+// arr = [9, 17, 2, 0, 19]        
+// for (index = 0; index < arr.length; index++) 
+// {      
+// console.log(array[index] + ” ”);    }
+var res = function (a, b) {
+    return a * b;
+};
+console.log(res(12, 2));
