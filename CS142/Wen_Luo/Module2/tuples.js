256da0aa9780048457f268ab832ae145e9066c3c
wenxin luo <wen@wenxins-MacBook-Pro.local>
2020-07-02 15:19:55 -0700

    Submission for Module2

---
+++ CS142/Wen_Luo/Module2/tuples.js
@@ -0,0 +1,19 @@
+"use strict";
+exports.__esModule = true;
+var tupleA = ['Hello Tuple'];
+var tupleB = [135, 'rgb', true];
+/// tuple C and D are equal
+var tupleC = [
+    [12, 34],
+    [56, 78, 25],
+    [90, 12]
+];
+var tupleD = [
+    [12, 34],
+    [56, 78, 25],
+    [90, 12]
+];
+// Rest element 
+var bools = [true, true, false, true, false];
+// A heterogeneous list 
+var info = ['Hello', 372, true, false, true, true];
