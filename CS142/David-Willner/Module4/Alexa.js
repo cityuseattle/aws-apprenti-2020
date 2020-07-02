902bb1703249b25379d842f7ef507ae326884e2d
DavidmWilner <david.m.willner@gmail.com>
2020-07-02 16:37:56 -0700

    Submission for Module 4

---
+++ CS142/David-Willner/Module4/Alexa.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.Alexa = void 0;
+var Alexa = /** @class */ (function () {
+    function Alexa() {
+    }
+    Alexa.play = function (music) {
+        console.log('Music:', music);
+    };
+    return Alexa;
+}());
+exports.Alexa = Alexa;
