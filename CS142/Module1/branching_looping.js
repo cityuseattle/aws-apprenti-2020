903ee1f0efdec835fb97551b98c4132774312640
lanelane132 <djlane19@gmail.com>
2020-07-06 13:46:41 -0700

    Module1

---
+++ CS142/Module1/branching_looping.js
@@ -0,0 +1,6 @@
+var animal = ['cat', 'dog'];
+if (animal[0] === animal[1])
+    console.log('A(n) ' + animal[0] + ' is a(n) ' + animal[1]);
+else {
+    console.log('A(n)' + animal[0] + ' is not a(n)' + animal[1]);
+}
---
+++ CS142/Module2/tuples.ts
@@ -0,0 +1,18 @@
+var tupleA = ['Hello World'];
+var tupleB = [135, 'rgb', true];
+// tupleC and tupleD are equivalent
+var tupleC = [
+    [12, 34],
+    [56, 78, 25],
+    [90, 12]
+];
+var tupleD = [
+    [12, 34],
+    [56, 78, 25],
+    [90, 12]
+];
+// Rest elements
+var bools = [true, true, false, true, false];
+// A heterogeneous list
+var info = ['Hello', 372, true, false, true, true];
+
