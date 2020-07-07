c2bba28a7eddf2043ad4fdace27175cd1ab93041
Hye Shin <hye.stanford2@gmail.com>
2020-07-07 11:04:01 -0700

    Submission for Module7

---
+++ CS142/hyeshin/Module7/Main.ts
@@ -0,0 +1,9 @@
+import {Instrument, play} from './DoSomthing';
+import {testMap} from './DoSomthing';
+import twoSum from './DoSomthing';
+
+play(Instrument.piano);
+testMap();
+console.log(twoSum([3,8,12,2], 11));
+
+
