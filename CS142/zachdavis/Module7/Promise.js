94be9b44f772119d622aad6fe76b1e4977625aca
zdvis <66082221+zdvis@users.noreply.github.com>
2020-07-07 13:02:56 -0700

    Submission for Module 7

---
+++ CS142/zachdavis/Module7/Promise.js
@@ -0,0 +1,19 @@
+var makePromise = (action) => {
+    return new Promise(function (resolve, reject) {
+        if (action === 'Study') {
+            resolve('Good job!');
+        }
+        else {
+            reject('Study harder.');
+        }
+    });
+};
+const checkPromise = (theAction) => makePromise(theAction).then(function (resolve) {
+    console.log(resolve);
+}, function (reject) {
+    console.log(reject);
+});
+var theAction = 'Skip class';
+checkPromise(theAction);
+theAction = 'Study';
+checkPromise(theAction);
