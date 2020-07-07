6814c38478c0a8fc119cbc6bd66a6ba1a8ff5242
Beau Harris <beau.harris@trojans.dsu.edu>
2020-07-07 16:03:33 -0700

    submitting work for module7

---
+++ CS142/beauharris/Module07/Promise.ts
@@ -0,0 +1,23 @@
+var makePromise = (action: string) => {
+    return new Promise(function (resolve, reject) {
+        if(action === 'Study') {
+            resolve('Good job!');
+        } else {
+            reject('Study harder.');
+        }
+    });
+};
+
+const checkPromise = (theAction: string) => makePromise(theAction).then(
+    function(resolve) {
+        console.log(resolve);
+    }, function (reject) {
+        console.log(reject);
+    }
+);
+
+var theAction = 'Skip class';
+checkPromise(theAction);
+
+theAction = 'Study';
+checkPromise(theAction);
