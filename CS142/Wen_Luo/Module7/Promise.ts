23c4b568c4344faaa5b30f4bf76962369ef41bce
wenxin luo <wen@wenxins-MacBook-Pro.local>
2020-07-07 15:00:14 -0700

    Submission for Module7

---
+++ CS142/Wen_Luo/Module7/Promise.ts
@@ -0,0 +1,31 @@
+// Creating Promises 
+
+var makePromise = (action: string) => {
+    return new Promise(function (resolve, reject) {
+        if ( action === 'Study') {
+            // if successful, resolve the promise 
+            resolve('GoodJOb ! ');
+        } else {
+            reject ('Study Harder.');
+        }
+    });
+};
+
+//Reusable function to check the promise made 
+
+const checkPromise = (theAction: string) => makePromise(theAction).then(
+    function (resolve) {
+        console.log(resolve);
+    }, function (reject) {
+        console.log(reject);
+    }
+);
+
+// check the promise 1st time 
+
+var theAction = 'Skip class';
+checkPromise(theAction);
+
+// second check 
+theAction = 'Study';
+checkPromise(theAction);
