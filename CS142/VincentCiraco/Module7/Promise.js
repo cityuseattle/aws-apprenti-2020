01aa888ba3102f6e4f18b438ff5c57266eefd68e
vincent <vinchenzociraco95@gmail.com>
2020-07-07 15:58:15 -0700

    cs142 Module7

---
+++ CS142/VincentCiraco/Module7/Promise.js
@@ -0,0 +1,19 @@
+var makePromise = (action) => {
+    return new Promise(function (resolve, reject) {
+        if (action === 'Study') {
+            resolve('Good Job!');
+        }
+        else {
+            reject('Study Harder');
+        }
+    });
+};
+const checkPromise = (theAction) => makePromise(theAction).then(function (resolve) {
+    console.log(resolve);
+}, function (reject) {
+    console.log(reject);
+});
+var theAction = 'Skip class';
+checkPromise(theAction);
+theAction = 'Study';
+checkPromise(theAction);
