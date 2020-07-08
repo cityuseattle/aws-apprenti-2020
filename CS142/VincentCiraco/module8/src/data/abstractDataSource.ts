e8f90356d7d581a08e9ccd145dc1ad6200bb5442
vincent <vinchenzociraco95@gmail.com>
2020-07-08 14:31:23 -0700

    cs142 Module8 HOP

---
+++ CS142/VincentCiraco/module8/src/data/abstractDataSource.ts
@@ -0,0 +1,25 @@
+import { Product } from "./entities";
+export type ProductProp = keyof Product; 
+export abstract class AbstractDataSource {
+    private _products: Product[];
+    public loading: Promise<void>;
+    constructor(){
+        this._products = [];
+        this.loading = this.getData();
+    }
+    async getproducts(sortProp: ProductProp = "id", 
+            category? : string): Promise<Product[]> {
+                await this.loading;
+                return this._products;
+            }
+    protected async getData(): Promise<void> {
+        this._products = [];
+        const rawData = await this.loadProducts();
+        rawData.forEach(p => {
+            this._products.push(p);
+        });
+            
+        
+    }
+    protected abstract loadProducts(): Promise<Product[]>;
+}
