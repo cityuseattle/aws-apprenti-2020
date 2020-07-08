b657adfb6f218eed40a83289db574880b27dd08d
DainJensen <danishterminator@gmail.com>
2020-07-08 10:27:15 -0700

    Submission for Module 5

---
+++ CS142/DainJensen/Module5/gt_array.ts
@@ -0,0 +1,9 @@
+function loggingIdentity<T>(arg: T[]): T[]  {
+    console.log(arg.length);  // Array has a .length, so no more error
+    return arg;
+}
+
+
+var names:string[] = new Array("A","B","C","D")
+loggingIdentity(names)
+
