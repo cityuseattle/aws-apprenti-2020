85301a5453134cbb4c256bcc25d9f1a0c6fc8324
lanelane132 <djlane19@gmail.com>
2020-07-08 14:55:31 -0700

    Module5

---
+++ CS142/Module5/generic_class1.js
@@ -0,0 +1,11 @@
+;
+var kvProcessor = /** @class */ (function () {
+    function kvProcessor() {
+    }
+    kvProcessor.prototype.process = function (key, val) {
+        console.log("Key = " + key + ", val =" + val);
+    };
+    return kvProcessor;
+}());
+var proc = new kvProcessor();
+proc.process(1, 'ABC');
