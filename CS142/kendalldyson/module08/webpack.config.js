0c78cfac48364d31ef2dc8829e99ab37320e0a50
Kendall Jay Dyson <kjdyson@Kendalls-MacBook-Air.local>
2020-07-09 01:19:39 -0700

    Submission for module08

---
+++ CS142/kendalldyson/module08/webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/index.ts",
+  output: { filename: "bundle.js"},
+  resolve: { extensions: [".ts", ".js"] },
+  module: {
+    rules: [
+      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ }
+    ]
+  },
+  devServer: {
+    contentBase: "./assets",
+    port: 4505
+  }
+   
+
+};
+
