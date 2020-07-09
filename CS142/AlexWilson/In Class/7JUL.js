5c60bec78ab6901b5a2edd94cae21be17f3a2ee8
Wilson <lexon@3c22fb582218.ant.amazon.com>
2020-07-09 05:30:15 -0700

    Submission HOP 10 WilsonAlex

--- CS142/AlexWilson/In Class/7JUL.js
+++ CS142/AlexWilson/In Class/7JUL.js
@@ -1,7 +1,6 @@
+"use strict";
 // public
-
-import { resolve } from "path";
-
+exports.__esModule = true;
 // class Employee {
 //     public empCode: number;
 //     empName: string;
@@ -9,9 +8,7 @@ import { resolve } from "path";
 // let emp = new Employee();
 // emp.empCode = 123;
 // emp.empName = "Swati";
-
 // private
-
 // class Employee {
 //     private empCode: number;
 //     empName: string;
@@ -19,13 +16,10 @@ import { resolve } from "path";
 // let emp = new Employee();
 // emp.empCode = 123;
 // emp.empName = "Swati";
-
 // protected
-
 // class Employee {
 //     public empName: string;
 //     protected empCode: number;
-
 //     constructor(name: string, code: number) {
 //         this.empName = name;
 //         this.empCode = code;
@@ -33,7 +27,6 @@ import { resolve } from "path";
 // }
 // class SalesEmployee extends Employee {
 //     private department: string;
-
 //     constructor(name: string, code: number, department: string) {
 //         super(name, code);
 //         this.department = department;
@@ -41,25 +34,19 @@ import { resolve } from "path";
 //     }
 // }
 // let emp = new SalesEmployee("John Smith", 123);
-
 // Generic Types
-
 // let arr = ['E line', 14, true];
 // function filter<T>(input: T) {
 //     console.log(typeof input);
 // }
 // arr.forEach(item => filter(item));
-
 // different section
 // let a = 1;
 // function testType <T>(input : T) {}
-
-
 //icallback
 // interface iCallback {
 //     ( error: Error, result?: number ) : void;
 // }
-
 // function getRandomNumber (callback: iCallback) : void {
 //     var value = Math.random();
 //     ( value >= 0.5)
@@ -67,7 +54,6 @@ import { resolve } from "path";
 //     : callback (new Error ("random number too low"))
 //     ;
 // }
-
 // for ( var i = 0; i < 10; i++ ) {
 //     getRandomNumber(
 //         (error : Error, result: number ) : void => {
@@ -75,22 +61,17 @@ import { resolve } from "path";
 //         }
 //     )
 // }
-
 // promise
-
 function asyncMethod(callBack) {
-    setTimeout(() => {
+    setTimeout(function () {
         console.log("Async Callback");
         callBack();
     }, 1500);
 }
-
-asyncMethod(() => console.log("Async Callback Completed"));
-
-var promise = new Promise ((resolve, reject) => {
-    setTimeout(() => {
-        console.log("Async is done")
+asyncMethod(function () { return console.log("Async Callback Completed"); });
+var promise = new Promise(function (resolve, reject) {
+    setTimeout(function () {
+        console.log("Async is done");
         resolve();
     }, 1500);
-    
 });
