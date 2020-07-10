bec2c9f6cf97a09a421978bc5c89e6df38700c32
vincent <vinchenzociraco95@gmail.com>
2020-07-10 14:20:59 -0700

    cs142 Module9

---
+++ CS142/VincentCiraco/Module9/Stack.ts
@@ -0,0 +1,34 @@
+class Stack<T>
+{
+    private collection: T[];
+    constructor()
+    {
+        this.collection = [];
+    }
+    push(item: T){
+        this.collection.push(item);
+    }
+    pop(){
+        if(this.collection.length !== 0)
+            this.collection.shift();
+        else
+            throw new Error('Trying to pop without items in it.');
+    }
+    top(){
+        if(this.collection.length !== 0)
+            return this.collection[0];
+        else    
+            return null;
+        
+    }
+}
+let stack = new Stack();
+stack.push(1);
+stack.push(2);
+stack.push(3);
+console.log(stack.top());
+stack.pop();
+console.log(stack.top());
+stack.pop();
+console.log(stack.top());
+stack.pop();
