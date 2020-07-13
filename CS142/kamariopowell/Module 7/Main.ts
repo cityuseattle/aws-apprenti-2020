d960451c7d784e580859b170ae462208ea569857
Powell <kamariop@amazon.com>
2020-07-13 01:20:26 -0500

    submission for Modle 7

---
+++ CS142/kamariopowell/Module 7/Main.ts
@@ -0,0 +1,7 @@
+import {Instrument, play} from'./DoSomethings';
+import {testMap} from'./DoSomethings';
+import twoSum from'./DoSomethings';
+
+play(Instrument.Piano);
+testMap();
+console.log(twoSum([3,8,12,2], 11));
