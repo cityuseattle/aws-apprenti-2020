18395ef07a088ffc82eb39fa21ee130c6f765fd6
Beau Harris <beau.harris@trojans.dsu.edu>
2020-07-27 11:25:04 -0700

    submitting work for module01a

---
+++ CS274/beauharris/Module01a/server-side/serverSide.js
@@ -0,0 +1,11 @@
+function calcProduct(a, b){
+    //function returns product of two numbers
+    return a * b;
+}
+console.log("Server-Side javaScript\n");
+console.log("Built-In Function");
+console.log("Display Date and Time");
+console.log(Date());
+console.log();
+console.log("User-Defined Function");
+console.log("A multiplication function: ", calcProduct(5,5));
