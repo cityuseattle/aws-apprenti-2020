bf4f5d534ba334d00c679f8843083999e1403b25
Kendall Jay Dyson <kjdyson@Kendalls-MacBook-Air.local>
2020-07-28 00:07:56 -0700

    Submission for Module 1

---
+++ CS274/kendalldyson/module01a/server-side/serverSide.js
@@ -0,0 +1,12 @@
+console.log("Server Side javascript");
+console.log();
+console.log("Built In Function");
+console.log("Display Date and Time");
+console.log(Date());
+
+function myFunction (p1, p2) {
+    return p1 * p2;
+}
+console.log();
+console.log("User Defined Function");
+console.log("A Multiplication function:", myFunction(6,3));
