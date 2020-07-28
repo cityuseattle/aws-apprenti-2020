f752a478cb0f7b6256cc58282facc84920f50291
Jean-Jacques Philippe Guerrier <jean-jacquesphilippeguerrier@Jean-Jacquess-MacBook-Pro.local>
2020-07-27 23:49:36 -0400

    Submission for Module 1

---
+++ CS274/GeraldGuerrier /Module01A/server-side/serverSide.js
@@ -0,0 +1,14 @@
+console.log
+console.log("Server-Side javascript");
+console.log();
+console.log("Built-in Function");
+console.log("Display Date and Time");
+console.log(Date());
+
+function myFunction(p1, p2) {
+    return p1 * p2;
+}
+
+console.log();
+console.log("User-Defined Function");
+console.log("A multiplication function: ", myFunction(6, 3));
