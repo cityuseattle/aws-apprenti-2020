e4406824d29068e41484dbdc5040724e1c372d54
Jean-Jacques Philippe Guerrier <jean-jacquesphilippeguerrier@Jean-Jacquess-MacBook-Pro.local>
2020-07-28 01:34:38 -0400

    Submission for Module 1

---
+++ CS274/GeraldGuerrier /Module01B/07 Formatting Practice/ListingD27.js
@@ -0,0 +1,16 @@
+console.log(`>>>Listing D.27`)
+
+const firstname = 'Simon';
+let surname;
+if (firstname === 'Simon'){
+    surname = 'Holmes';
+    console.log(`${firstname} ${surname}`);
+}
+
+function returnFullname() {
+    var name = 'Gerald Guerrier';
+    return `{ name : '${name}' }`
+
+  }
+  
+console.log (returnFullname());
