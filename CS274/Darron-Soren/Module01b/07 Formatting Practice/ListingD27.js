fa27f900755c558d70276ced40cc8aef9e26ffbf
Darron Soren <cpttacsat@gmail.com>
2020-07-28 18:52:38 -0400

    Submission for Modiule01b

---
+++ CS274/Darron-Soren/Module01b/07 Formatting Practice/ListingD27.js
@@ -0,0 +1,14 @@
+console.log('>>>Listing D.27')
+
+const firstname = 'Simon';
+let surname;
+if (firstname === 'Simon') {
+    surname = "Holmes";
+    console.log(`${firstname} ${surname}`);
+}
+
+if (firstname === 'Simon')
+{
+    surname = 'Holmes';
+    console.log(`${firstname} ${surname}`);    
+}
---
+++ CS274/Darron-Soren/Module01b/07 Formatting Practice/ListingD27a.js
@@ -0,0 +1,9 @@
+console.log('>>>Listing D.27')
+
+const firstname = 'Simon';
+let surname;
+if (firstname === 'Simon') {
+    surname = "Holmes";
+    console.log(`${firstname} ${surname}`);
+}
+
