3011948a3bf29ab6cc51b640547e911226636edb
Robert Kennedy <66082154+rjkennedy707@users.noreply.github.com>
2020-07-28 11:10:05 -0400

    Submission for Module 1

---
+++ CS274/RobertKennedy/module01b/02Scope/ListingD08b.js
@@ -0,0 +1,11 @@
+console.log(`>>>Listing D.8b`)
+
+let firstname = 'Simon';
+const addSurname = function () {
+    let surname,
+        fullname;
+    surname = 'Holmes';
+    fullname = firstname + ' ' + surname;
+    console.log(fullname);
+};
+addSurname();
