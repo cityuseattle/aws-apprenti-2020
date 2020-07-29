9371098dc67f21e58b77f4e7a194f0fb7909662b
wenxin luo <wen@wenxins-MacBook-Pro.local>
2020-07-29 14:48:20 -0700

    Submission for module03c

---
+++ CS274/Wen_Luo/module03c/app_server/controllers/main.js
@@ -0,0 +1,12 @@
+/* This get homepage controller */
+// 1. create an index function 
+// 2. Include controller code for the homepage 
+
+const index = (req, res) => {
+    res.render('index', {title: 'Express - Chapter 3'});
+};
+
+//3. Expose the index function as a method. 
+module.exports = {
+    index
+};
