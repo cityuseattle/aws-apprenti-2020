ecd62e24d126cfd55a03eec8fc135d9ac4409576
Shinerj <jshiner2018@gmail.com>
2020-07-29 14:05:04 -0700

    Module3a

---
+++ CS274/Jesse_Shiner/module01b/02 Scope/ListingD08.js
@@ -0,0 +1,9 @@
+console.log('>>>Listing D.8')
+var firstname = 'Simon';
+var addSurname = function(){
+    var surname = 'Holmes';
+    var firstname = 'David';
+    var fullname = firstname + ' ' + surname;
+    console.log(fullname);
+};
+addSurname();
