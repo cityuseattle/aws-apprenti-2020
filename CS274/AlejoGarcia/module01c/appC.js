fc22212ab549d69b8cde89a9cda60c28173b6725
AJ Garcia <amgarcia.mil@gmail.com>
2020-07-29 08:20:35 -0700

    Submission for Module 3a

--- CS274/AlejoGarcia/module01c/appC.js
+++ CS274/AlejoGarcia/module01c/appC.js
@@ -13,11 +13,11 @@ const server = http.createServer((req, res) => {
   <p>This example calls a function which performs a calculation, and returns the result: </p>\
   <p id="demo"></p>\
   <script>\
-  function myFunction(p1, p2) {\
+    function myFunction(p1, p2) {\
       return p1 * p2;\
-  }\
-  document.getElementById("demo").innerHTML = myFunction(10, 5);\
-</script>'
+    }\
+    document.getElementById("demo").innerHTML = myFunction(10, 5);\
+  </script>'
   res.end(output);
 
   
