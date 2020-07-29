27e0aa0b987dc3d143c9137549d629b8b3f78baf
AJ Garcia <amgarcia.mil@gmail.com>
2020-07-29 09:23:11 -0700

    Submission for Module 3b

---
+++ CS274/AlejoGarcia/module03b/app_server/routes/index.js
@@ -0,0 +1,17 @@
+// var express = require('express');
+// var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+
+/* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// 1. Require the main controllers file.
+const ctrlMain = require('../controllers/main');
+// 2. Reference the index method of the controllers i nthe route definition.
+router.get('/', ctrlMain.index);
+
+module.exports = router;
