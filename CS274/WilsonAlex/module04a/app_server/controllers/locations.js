d88f1656af445fe9ee196cb2be41a60e9c2b6cb0
Wilson <lexon@3c22fb582218.ant.amazon.com>
2020-07-30 12:54:55 -0700

    Submission HOP4a WilsonAlex

---
+++ CS274/WilsonAlex/module04a/app_server/controllers/locations.js
@@ -0,0 +1,22 @@
+// GET 'home' page
+
+const homelist = (req, res) => {
+    res.render('index', { title: 'Home' });
+};
+
+// GET 'Location info' page
+const locationInfo = (req, res) => {
+    res.render('index', { title: 'Location info' });
+};
+
+// GET 'review' page
+const addReview = (req, res) => {
+    res.render('index', { title: 'Add review' });
+};
+
+module.exports = {
+    homelist,
+    locationInfo,
+    addReview
+};
+
