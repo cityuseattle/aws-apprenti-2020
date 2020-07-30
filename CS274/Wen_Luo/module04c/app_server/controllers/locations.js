ac2794195b5e811c36c6049494eed68af810de52
wenxin luo <wen@wenxins-MacBook-Pro.local>
2020-07-30 16:18:12 -0700

    Submission for module04c

---
+++ CS274/Wen_Luo/module04c/app_server/controllers/locations.js
@@ -0,0 +1,20 @@
+// Get homepage //
+const homelist = (req, res) => {
+    res.render('locations-list', {title: 'Home' });
+};
+
+// Get 'Locations Info' page //
+const LocationInfo = (req, res) => {
+    res.render('index', {title: 'Locations'});
+};
+
+// Get add review page //
+const addReview = (req, res) => {
+    res.render ('index', {title: 'Add Review'});
+};
+
+module.exports = {
+    homelist,
+    LocationInfo,
+    addReview
+};
