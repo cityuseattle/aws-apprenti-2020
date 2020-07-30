24ecfc281300d00fdabca410e4a42de87de6c389
juanc <chavesjuan@cityuniversity.edu>
2020-07-30 11:47:21 -0700

    Module 4B Submission

---
+++ CS274/juanchaves/Module04B/app_server/controllers/locations.js
@@ -0,0 +1,21 @@
+/* GET 'home' page */
+const homelist = (req, res) => {
+  res.render('index', { title: 'Home' });
+};
+
+/* GET 'Location info' page */
+const locationInfo = (req, res) => {
+  res.render('index', { title: 'Location info' });
+};
+
+/* GET 'Add review' page */
+const addReview = (req, res) => {
+  res.render('index', { title: 'Add review' });
+};
+
+  // 3. Expose the index function as a method.
+module.exports = {
+  homelist,
+  locationInfo,
+  addReview
+};
