eb5fc08b02007e8af9682f6cd89870b9a336d37d
juanc <chavesjuan@cityuniversity.edu>
2020-07-31 19:14:20 -0700

    Module 5C sub

--- CS274/juanchaves/Module05/app_server/controllers/locations.js
+++ CS274/juanchaves/Module05/app_server/controllers/locations.js
@@ -1,6 +1,32 @@
 /* GET 'home' page */
 const homelist = (req, res) => {
-  res.render('location-list', { title: 'Home' });
+  res.render('location-list', {
+    title: 'Home',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!'
+    },
+    sidebar: 'Loc8r helps you find places to work when out and about.',
+    locations: [{
+      name:         'Starcups',
+      address:     '125 High Street, Reading, RG6 1PS',
+      rating:       3,
+      facilities:   ['Hot drinks', 'Food', 'Premium wifi'],
+      distance:     '100m'
+    },{
+      name:         'Cafe Hero',
+      address:     '126 High Street, Reading, RG6 1PS',
+      rating:       4,
+      facilities:   ['Hot drinks', 'Food', 'Premium wifi'],
+      distance:     '200m'
+    },{
+      name:         'Burguer Queen',
+      address:     '127 High Street, Reading, RG6 1PS',
+      rating:       2,
+      facilities:   ['Food', 'Premium wifi'],
+      distance:     '250m'
+    }]
+  });
 };
 
 /* GET 'Location info' page */
