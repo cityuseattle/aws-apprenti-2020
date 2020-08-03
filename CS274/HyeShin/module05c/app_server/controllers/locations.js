19f08f1a378189cd4bdad865771a0024d2699051
Hye Shin <hye.stanford2@gmail.com>
2020-08-03 10:20:50 -0700

    Submission for Module05c

---
+++ CS274/HyeShin/module05c/app_server/controllers/locations.js
@@ -0,0 +1,58 @@
+/* GET 'home' page */
+const homelist = (req, res) => {
+    res.render('locations-list', { 
+        title: 'Home',
+        pageHeader: {
+            title: 'Loc8r',
+            strapline: 'Find places to work with wifi near you!'
+        },
+        sidebar: "Loc8r helps you find places to work when out and about.",
+        locations: [
+            {
+              name: 'Starcups',
+              address: '125 High Street, Reading, RG6 1PS',
+              rating: 3,
+              facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+              distance: '100m'
+            },
+            {
+              name: 'Cafe Hero',
+              address: '125 High Street, Reading, RG6 1PS',
+              rating: 4,
+              facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+              distance: '200m'
+            },
+            {
+              name: 'Burger Queen',
+              address: '125 High Street, Reading, RG6 1PS',
+              rating: 2,
+              facilities: ['Food', 'Premium wifi'],
+              distance: '250m'
+            }
+          ]
+        }
+      );
+    };
+
+/* GET 'Location info' page */
+//const locationInfo = (req, res) => {
+//    res.render('index', { title: 'Location info' });
+//};
+
+const locationInfo = (req, res) => {
+    res.render('location-info', { title: 'Location info' });
+};
+
+/* GET 'Add review' page */
+const addReview = (req, res) => {
+    res.render('location-review-form', { 
+        title: 'Review Starcups on Loc8r',
+        pageHeader: { title: 'Review Starcups' } 
+    });
+};
+
+module.exports = {
+    homelist,
+    locationInfo,
+    addReview
+};
