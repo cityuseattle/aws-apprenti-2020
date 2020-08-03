055550467417ebe74e3784820c4fd75a092d1447
Robert Kennedy <66082154+rjkennedy707@users.noreply.github.com>
2020-08-03 18:19:25 -0400

    Submission for HOP05B

--- CS274/RobertKennedy/module05b/app_server/controllers/locations.js
+++ CS274/RobertKennedy/module05b/app_server/controllers/locations.js
@@ -1,21 +1,20 @@
-/* GET 'home' page */
-const homelist = (req, res) => {
-    res.render('location-list', { title: 'Home' });
-  };
-  
-  /* GET 'Location info' page */
-  const locationInfo = (req, res) => {
-    res.render('location-info', { title: 'Location info' });
-  };
-  
-  /* GET 'Add review' page */
-  const addReview = (req, res) => {
-    res.render('location-review-form', { title: 'Add review' });
-  };
-  
-    // 3. Expose the index function as a method.
-  module.exports = {
+const homelist = (req, res)=> {res.render('locations-list', { title: 'Home' });
+};
+
+const locationInfo = (req, res) => {
+    res.render('location-info',{title: 'Location Info'});
+};
+
+const addReview = (req, res) => {
+    res.render('location-review-form', {
+        title: "Review Starcups on Loc8r",
+        pageHeader: {title: 'Review Starcups'}
+
+    });
+};
+
+module.exports = {
     homelist,
     locationInfo,
     addReview
-  };
+};
