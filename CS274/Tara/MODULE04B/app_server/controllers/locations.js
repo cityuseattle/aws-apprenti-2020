e092ff08683372f16306c7c83c990d30ff507ba1
TARA BAHADUR PUN <pun.tara@outlook.com>
2020-08-02 18:30:06 -0700

    Submission for Module 04 B HOP

---
+++ CS274/Tara/MODULE04B/app_server/controllers/locations.js
@@ -0,0 +1,18 @@
+/* GET 'home' page */
+const homelist =(req, res)=> {
+    res.render('index', { title: 'Home'});
+};
+/* GET 'Location info' page */
+const locationInfo =(req, res)=> {
+    res.render('index', { title: 'Location info'});
+};
+/* GET 'Add review' page */
+const addReview =(req, res)=> {
+    res.render('index', { title: 'Add review'});
+};
+// this module is exported to router
+module.exports= {
+    homelist, 
+    locationInfo,
+    addReview
+};
