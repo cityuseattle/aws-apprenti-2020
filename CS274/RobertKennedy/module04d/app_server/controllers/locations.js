a84bdafe469a0ebedf1a48f24b2ff097938f7819
Robert Kennedy <66082154+rjkennedy707@users.noreply.github.com>
2020-08-03 16:49:43 -0400

    Submission for Module 6

---
+++ CS274/RobertKennedy/module04d/app_server/controllers/locations.js
@@ -0,0 +1,22 @@
+/* GET 'home' page */
+const homelist = (req, res) => {
+    res.render('location-list', { title: 'Home' });    
+};
+
+
+/* GET 'Location info' page */
+const locationInfo = (req, res) => {
+    res.render('location-info', { title: 'Location info' });    
+};
+
+
+/* GET 'add review' page */
+const addReview = (req, res) => {
+    res.render('location-review-form', { title: 'Add review' });    
+};
+
+module.exports = {
+    homelist,
+    locationInfo,
+    addReview
+};
