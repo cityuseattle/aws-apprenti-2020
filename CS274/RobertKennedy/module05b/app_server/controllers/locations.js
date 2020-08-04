67de761d78685ea207912329e299785a21288560
Robert Kennedy <66082154+rjkennedy707@users.noreply.github.com>
2020-08-04 14:51:12 -0400

    HOP5B

--- CS274/RobertKennedy/module05b/app_server/controllers/locations.js
+++ CS274/RobertKennedy/module05b/app_server/controllers/locations.js
@@ -1,20 +1,30 @@
-const homelist = (req, res)=> {res.render('locations-list', { title: 'Home' });
+/* Get 'home' page */
+const homelist = (req, res) => {
+  res.render('locations-list', { title: 'Home'});
 };
 
+/* Get 'Location info' page */
+// const locationInfo = (req, res) => {
+//     res.render('index', { title: 'Location info'});
+// };
 const locationInfo = (req, res) => {
-    res.render('location-info',{title: 'Location Info'});
+  res.render('location-info', { title: 'Location info'});
 };
 
-const addReview = (req, res) => {
-    res.render('location-review-form', {
-        title: "Review Starcups on Loc8r",
-        pageHeader: {title: 'Review Starcups'}
-
-    });
+/* Get 'Add review' page */
+// const addReview = function(req, res) {
+//     res.render('location-review-form', { title: 'Review Starcups on Loc8r', pageHeader: { title: 'Review Starcups' }
+//     });
+// };
+const addReview = function(req, res){
+  res.render('location-review-form', {
+    title: 'Review Starcups on Loc8r',
+    pageHeader: { title: 'Review Starcups' }
+  });
 };
 
 module.exports = {
-    homelist,
-    locationInfo,
-    addReview
+  homelist,
+  locationInfo,
+  addReview
 };
