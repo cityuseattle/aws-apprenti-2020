e67df88051118c016f5c3fbf7460da3174da8d77
Hye Shin <hye.stanford2@gmail.com>
2020-08-04 14:23:51 -0700

    Submission for Module 6

---
+++ CS274/HyeShin/module6/app_server/controllers/others.js
@@ -0,0 +1,16 @@
+/* GET homepage controller */
+// 1. Create an index function. 
+// 2. Include controller code for the homepage. 
+
+/*GET 'about/ page*/
+const about = (req, res) => {
+    res.render('generic-text', { 
+        title: 'About Loc8r',
+        content: 'This is the about page.'
+     });
+};
+
+// 3. Expose the index function as a method.
+module.exports = {
+    about
+};
