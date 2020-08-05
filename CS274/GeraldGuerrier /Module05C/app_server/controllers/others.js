d4e6e953ff69c37936879801f09baec4d658e0f1
Jean-Jacques Philippe Guerrier <jean-jacquesphilippeguerrier@Jean-Jacquess-MacBook-Pro.local>
2020-08-05 15:45:20 -0400

    Submission Module05C

---
+++ CS274/GeraldGuerrier /Module05C/app_server/controllers/others.js
@@ -0,0 +1,9 @@
+/* GET about page */
+const about = (req, res) => {
+  res.render('generic-text', { title: 'About- Chapter 4' });
+};
+
+// 3. Expose the index function as a method.
+module.exports = {
+  about
+};
