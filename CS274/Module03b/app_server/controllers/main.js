a31d0355437c7a19b2fdb5eaefa097f9762f2adb
lanelane132 <djlane19@gmail.com>
2020-08-06 13:51:51 -0700

    app_server

---
+++ CS274/Module03b/app_server/controllers/main.js
@@ -0,0 +1,11 @@
+/* Get homepage controller */
+// 1. Create an index function.
+// 2. Include controller code for the homepage.
+const index = (req, res) => {
+    res.render('index', { title: 'Express - Chapter 3'});
+}
+
+// 3 Expose the index function as method.
+module.exports = {
+ index
+}  
