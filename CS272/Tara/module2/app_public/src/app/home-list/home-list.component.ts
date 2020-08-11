c3a55f1f1adfbd3ad1dda0f94d1db93ec40615dc
TARA BAHADUR PUN <pun.tara@outlook.com>
2020-08-11 15:30:39 -0700

    Submission for module 2 HOP

---
+++ CS272/Tara/module2/app_public/src/app/home-list/home-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+export class Location {
+  _id: string;
+  name: string;
+  distance: number;
+  address: string;
+  rating: number;
+  facilities: string[];
+}
+@Component({
+  selector: 'app-home-list',
+  templateUrl: './home-list.component.html',
+  styleUrls: ['./home-list.component.css']
+})
+export class HomeListComponent implements OnInit {
+
+  constructor() { }
+  locations: Location[]=[{
+    _id:'5f309647cab5bc8deb8dbfa6',
+  name:'Starcups',
+  distance: 14000.1234,
+  address:'High Street, Reading',
+  rating: 5,
+  facilities:['wifi', 'food','hot drinks']
+  },
+  {
+    _id: '590d8dc7a7cb5b8e3f1bfc48',
+  name: 'Costy',
+  distance: 120.1234,
+  address: 'High Street, Reading',
+  rating: 3,
+  facilities: ['hot drinks', 'food', 'power']
+  }];
+  
+  
+  ngOnInit(): void {
+  }
+
+}
