c90b7d7c1cab481c65b5fd1d9e030049cb61bd32
Robert Kennedy <66082154+rjkennedy707@users.noreply.github.com>
2020-08-12 12:49:46 -0400

    Submission for HOP05D

---
+++ CS272/RobertKennedy/module02/app_public/src/app/home-list/home-list.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+
+export class Location {
+  _id: string;
+  name: String;
+  distance: number;
+  address: string;
+  rating: number;
+  facilities: string[];
+}
+
+@Component({
+  selector: 'app-home-list',
+  templateUrl: './home-list.component.html',
+  styleUrls: ['./home-list.component.css']
+})
+export class HomeListComponent implements OnInit {
+  constructor() { }
+
+  locations: Location[] = [{
+    _id: '590d8dc7a7cb5b8e3f1bfc48',
+    name: 'Costy',
+    distance: 14000.1234,
+    address: 'High Street, Reading',
+    rating: 3,
+    facilities: ['Hot Drinks', 'Food', 'Power']
+  }, {
+    _id: '590d8dc7a7cb5b8e3f1bfc48',
+    name: 'Starcups',
+    distance: 120.542,
+    address: 'High Street, Reading',
+    rating: 5,
+    facilities: ['Wifi', 'Food', 'Hot Drinks']
+  }];
+
+  name = 'Costy Test';
+
+  ngOnInit(): void {
+  }
+}
