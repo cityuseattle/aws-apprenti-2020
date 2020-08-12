e98edcd98f6db6653b61a3c2ae176b87546f471d
DavidmWilner <david.m.willner@gmail.com>
2020-08-11 21:02:51 -0700

    Module02B Submission

---
+++ CS272/Module02B/app_public/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { HomeListComponent } from './home-list/home-list.component';
+import { DistancePipe } from './distance.pipe';
+
+@NgModule({
+  declarations: [
+    HomeListComponent,
+    DistancePipe
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [HomeListComponent]
+})
+export class AppModule { }
