64d4db3cae7f1edfd19b34a6ab48f5feb9cd834a
Robert Kennedy <66082154+rjkennedy707@users.noreply.github.com>
2020-08-12 17:30:18 -0400

    Submission for HOP05D

--- CS272/RobertKennedy/module03a/app_public/src/app/home-list/home-list.component.ts
+++ CS272/RobertKennedy/module03a/app_public/src/app/home-list/home-list.component.ts
@@ -16,26 +16,17 @@ export class Location {
   styleUrls: ['./home-list.component.css']
 })
 export class HomeListComponent implements OnInit {
-  constructor() { }
+  constructor(private Loc8rDataService: Loc8rDataService) { }
 
-  locations: Location[] = [{
-    _id: '590d8dc7a7cb5b8e3f1bfc48',
-    name: 'Costy',
-    distance: 14000.1234,
-    address: 'High Street, Reading',
-    rating: 3,
-    facilities: ['Hot Drinks', 'Food', 'Power']
-  }, {
-    _id: '590d8dc7a7cb5b8e3f1bfc48',
-    name: 'Starcups',
-    distance: 120.542,
-    address: 'High Street, Reading',
-    rating: 5,
-    facilities: ['Wifi', 'Food', 'Hot Drinks']
-  }];
+  public locations: Location[];
 
-  name = 'Costy Test';
+  private getLocations(): void {
+    this.Loc8rDataService
+    .getLocations()
+      .then(foundLocations => this.locations = foundLocations);
+  }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    this.getLocations();
   }
 }
