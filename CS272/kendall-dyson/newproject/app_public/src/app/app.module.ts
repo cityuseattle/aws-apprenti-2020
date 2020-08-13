36e2f4479ddad9edd48b75481bd9959f614245bd
Kendall Jay Dyson <kjdyson@Kendalls-MacBook-Air.local>
2020-08-13 13:38:47 -0700

    submission for 3b

--- CS272/kendall-dyson/newproject/app_public/src/app/app.module.ts
+++ CS272/kendall-dyson/newproject/app_public/src/app/app.module.ts
@@ -1,15 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
-    FrameworkComponent
+    FrameworkComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule
+    RouterModule.forRoot([ 
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [FrameworkComponent]
