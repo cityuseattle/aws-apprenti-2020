f182da985ac444a0ae292594c446bf2c9c506c78
Beau Harris <beau.harris@trojans.dsu.edu>
2020-08-13 09:58:03 -0700

    submitting work for module 04a

---
+++ CS272/beauharris/module04a/app_public/src/app/app.module.ts
@@ -0,0 +1,17 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+//import { AppComponent } from './app.component';
+import { FrameworkComponent } from './framework/framework.component';
+
+@NgModule({
+  declarations: [
+    FrameworkComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [FrameworkComponent]
+})
+export class AppModule { }
