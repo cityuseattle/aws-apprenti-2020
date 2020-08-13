ff27604232c8bbc29b7628e454645d55efe01619
TARA BAHADUR PUN <pun.tara@outlook.com>
2020-08-13 15:54:17 -0700

    Submission for Mudule 3 HOP

---
+++ CS272/Tara/module3/app_public/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { HomeListComponent } from './home-list/home-list.component';
+import { DistancePipe } from './distance.pipe';
+import { HttpClientModule } from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    HomeListComponent,
+    DistancePipe
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [HomeListComponent]
+})
+export class AppModule { }
