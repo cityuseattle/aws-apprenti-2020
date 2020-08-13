74657412d216ca4942b87d80c7b8759a8ba78a78
Wilson <lexon@3c22fb582218.ant.amazon.com>
2020-08-13 10:17:27 -0700

    submmission HOP4a WilsonAlex

---
+++ CS272/WilsonAlex/HOP4a/app_public/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { FrameworkComponent } from './framework/framework.component';
+
+@NgModule({
+  declarations: [
+
+    FrameworkComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [FrameworkComponent]
+})
+export class AppModule { }
