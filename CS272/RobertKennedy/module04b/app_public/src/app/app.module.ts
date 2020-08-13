4e91efe540eeeab8cf19b1fee2c39422cd5a751a
Robert Kennedy <66082154+rjkennedy707@users.noreply.github.com>
2020-08-13 16:48:10 -0400

    Submission for HOP05D

---
+++ CS272/RobertKennedy/module04b/app_public/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+
+import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
+@NgModule({
+  declarations: [
+    FrameworkComponent,
+    AboutComponent
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [FrameworkComponent]
+})
+export class AppModule { }
