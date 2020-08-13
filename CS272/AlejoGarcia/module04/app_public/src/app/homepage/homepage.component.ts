c9f9d504a2b20a3fa562cad49b215009f9989b12
AJ Garcia <amgarcia.mil@gmail.com>
2020-08-13 13:24:21 -0700

    Submission for Module 4

---
+++ CS272/AlejoGarcia/module04/app_public/src/app/homepage/homepage.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Loc8rDataService } from '../loc8r-data.service';
+
+
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
