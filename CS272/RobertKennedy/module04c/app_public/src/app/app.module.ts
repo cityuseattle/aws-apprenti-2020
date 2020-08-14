05453574be06f7ce006b7e9f8096e0b9a2e61fe5
Robert Kennedy <66082154+rjkennedy707@users.noreply.github.com>
2020-08-14 13:22:24 -0400

    Submission for HOP05D

---
+++ CS272/RobertKennedy/module04c/app_public/src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+
+import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HomeListComponent } from './home-list/home-list.component';
+import { DistancePipe } from './distance.pipe';
+
+@NgModule({
+  declarations: [
+    FrameworkComponent,
+    AboutComponent,
+    HomepageComponent,
+    HomeListComponent,
+    DistancePipe
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: HomepageComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ])
+  ],
+  providers: [],
+  bootstrap: [FrameworkComponent]
+})
+export class AppModule { }
