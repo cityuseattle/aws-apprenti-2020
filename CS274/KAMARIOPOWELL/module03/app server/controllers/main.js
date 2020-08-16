45f37dd27ed9a874717953187df36d6dd43ad12d
Powell <kamariop@amazon.com>
2020-08-16 12:24:09 -0400

    submission for module 6

--- CS274/KAMARIOPOWELL/module03/app server/controllers/main.js
+++ CS274/KAMARIOPOWELL/module03/app server/controllers/main.js
@@ -2,10 +2,10 @@
 // 1. Create an index function.
 // 2. Include controller code for the homepage.
 const index = (req, res) => {
-    res.render('index', { title: 'Express - Chapter 3' });
+  res.render('index', { title: 'Express - Chapter 3' });
 };
 
 // 3 Expose the index function as a method.
 module.exports = {
-    index
+ index
 };
