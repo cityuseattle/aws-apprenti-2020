9425e295cf94410b1d11ecc274bac3d6282f3bd0
Wilson <lexon@3c22fb582218.ant.amazon.com>
2020-08-17 05:56:21 -0700

    submission HOP5b wilsonAlex

---
+++ CS272/WilsonAlex/HOP5b/app_public/src/app/homepage/homepage.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css']
+})
+export class HomepageComponent implements OnInit {
+
+  constructor() { }
+
+  public pageContent = {
+    header: {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!'
+    },
+    sidebar : 'This is the sidebar; how big should it be?'
+  };
+
+  ngOnInit(): void {
+  }
+
+}
