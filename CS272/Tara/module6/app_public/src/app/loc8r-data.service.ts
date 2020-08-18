bdb942dfa04353db8b414373a19081b20970d2e7
TARA BAHADUR PUN <pun.tara@outlook.com>
2020-08-18 09:13:27 -0700

    submission for module 6 hop

---
+++ CS272/Tara/module6/app_public/src/app/loc8r-data.service.ts
@@ -0,0 +1,43 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Location } from './location';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class Loc8rDataService {
+  constructor(private http: HttpClient) { }
+ private apiBaseUrl = 'http://localhost:3000/api';
+ //private apiBaseUrl = environment.apiBaseUrl;
+  public getLocations(lat: number, lng: number): Promise<Location[]> {                              
+    const maxDistance: number = 50000;                               
+    const url: string = `${this.apiBaseUrl}/locations?lng=${lng}&lat=${lat}&maxDistance=${maxDistance}`; 
+    return this.http                                              
+      .get(url)                                                   
+      .toPromise()                                               
+      .then(response => response as Location[])                   
+      .catch(this.handleError);
+  }
+  public getLocationById(locationId: string): Promise<Location> {                                                            
+    const url: string = `${this.apiBaseUrl}/locations/${locationId}`; 
+    return this.http                                              
+      .get(url)                                                   
+      .toPromise()                                               
+      .then(response => response as Location)                   
+      .catch(this.handleError);
+  }
+
+  public addReviewByLocationId(locationId: string, formData: any): Promise<any> {
+    const url: string = `${this.apiBaseUrl}/locations/${locationId}/reviews`;
+    return this.http
+      .post(url, formData)
+      .toPromise()
+      .then(response => response as any)
+      .catch(this.handleError);
+  }
+  private handleError(error: any): Promise<any> {                 
+    console.error('Something has gone wrong', error);             
+    return Promise.reject(error.message || error);                
+  }
+}
