5eee82d33f36222703c8e90f95a1afb85f457139
zdvis <66082221+zdvis@users.noreply.github.com>
2020-08-18 16:35:06 -0700

    HOP06 and HOP07

---
+++ CS272/zachdavis/Module07/app_public/src/app/details-page/details-page.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { AppDataService } from '../loc8r-data.service';
+import { Location } from '../location';
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { switchMap } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-details-page',
+  templateUrl: './details-page.component.html',
+  styleUrls: ['./details-page.component.css']
+})
+export class DetailsPageComponent implements OnInit {
+
+  constructor(
+    private appDataService: AppDataService,
+    private route: ActivatedRoute
+    ) { }
+
+  public newLocation: Location;
+
+  ngOnInit(): void {
+  this.route.paramMap
+    .pipe(
+      switchMap((params: ParamMap) => {
+         let id = params.get('locationId');
+         return this.appDataService.getLocationById(id);
+       })
+    )
+    .subscribe((newLocation: Location) => {
+      this.newLocation = newLocation;
+      this.pageContent.header.title = newLocation.name;
+      this.pageContent.sidebar = `${newLocation.name} is on Loc8r
+      because it has accessible wifi and space to sit down with
+      your laptop and get some work done.`;
+    });
+}
+
+  public pageContent = {
+    header : {
+      title : '',
+      strapline : ''
+    },
+    sidebar : ''
+  };
+}
---
+++ CS272/zachdavis/Module07/app_public/src/app/location.ts
@@ -0,0 +1,22 @@
+class OpeningTimes {                1
+  days: string;
+  opening: string;
+  closing: string;
+  closed: boolean;
+}
+
+export class Review {
+  author: string;
+  rating: number;
+  reviewText: string;
+}
+
+export class Location {
+  _id: string;
+  name: string;
+  distance: number;
+  address: string;
+  rating: number;
+  facilities: string[];
+  reviews: any[];
+}
