0ad801f78b9da7467e9912190adc6f1a85d4e6c2
Lane <landerre@amazon.com>
2020-08-27 11:40:59 -0400

    Submission for HOP05D

--- CS272/derrelllane/module04/app_public/src/app/app.module.ts
+++ CS272/derrelllane/module04/app_public/src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {RouterModule } from '@angular/router';
 
 import { FrameworkComponent } from './framework/framework.component';
+import { AboutComponent } from './about/about.component';
+import { HtmlLineBreaksPipe } from './html-line-breaks.pipe';
 
 @NgModule({
   declarations: [
-    FrameworkComponent
+    FrameworkComponent,
+    AboutComponent,
+    HtmlLineBreaksPipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: 'about',
+        component: AboutComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [FrameworkComponent]
