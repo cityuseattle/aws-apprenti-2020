5892510dbd7fa7dda3121b178a32a1ea57784931
Lane <landerre@amazon.com>
2020-08-28 02:33:30 -0400

    Submission for HOP05D

--- CS272/derrelllane/module04/app_public/src/app/about/about.component.ts
+++ CS272/derrelllane/module04/app_public/src/app/about/about.component.ts
@@ -14,8 +14,8 @@ export class AboutComponent implements OnInit {
       title : 'About Loc8r',
       strapline : ''
     },
-    content : "Loc8r was created to help people find places to sit down and get a bit"
-  }
+    content : "Loc8r was created to help people find places to sit down and get a bit of work done."
+  };
 
   ngOnInit(): void {
   }
--- CS272/derrelllane/module04/app_public/src/app/app.module.ts
+++ CS272/derrelllane/module04/app_public/src/app/app.module.ts
@@ -5,12 +5,13 @@ import {RouterModule } from '@angular/router';
 import { FrameworkComponent } from './framework/framework.component';
 import { HtmlLineBreaksPipe } from './html-line-breaks.pipe';
 import { HomepageComponent } from './homepage/homepage.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
   declarations: [
     FrameworkComponent,
-    HtmlLineBreaksPipe,
-    HomepageComponent
+    AboutComponent,
+    HtmlLineBreaksPipe
   ],
   imports: [
     BrowserModule,
